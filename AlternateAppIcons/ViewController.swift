8e85d52c712d16ad99c8b92f001201c2ee87e46a
Sameh Mabrouk <smabrouk@mobiquityinc.com>
2017-01-29 19:26:00 +0100

    Initial commit

--- AlternateAppIcons/ViewController.swift
+++ AlternateAppIcons/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    
+    @IBAction func segmentdControlValueChanged(_ sender: Any) {
+        
+        guard UIApplication.shared.supportsAlternateIcons else {
+            let errorAlert = UIAlertController(title: "Error", message: "Sorry your device don't support Alternate icons", preferredStyle: .alert)
+            errorAlert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.show(errorAlert, sender: nil)
+            
+            return
+        }
+        
+        var iconName : String? = nil
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 1:
+            iconName = "fr"
+        case 2:
+            iconName = "de"
+        case 3:
+            iconName = "it"
+        default:
+            break
+        }
+        
+        UIApplication.shared.setAlternateIconName(iconName) { (error) in
+            if let error = error {
+                print("error: \(error)")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var selectedIndex = 0
+        
+        let currentIcon = UIApplication.shared.alternateIconName ?? ""
+        
+        switch currentIcon {
+        case "fr":
+            selectedIndex = 1
+        case "de":
+            selectedIndex = 2
+        case "it":
+            selectedIndex = 3
+        default:
+            break
+        }
+        
+        segmentedControl.selectedSegmentIndex = selectedIndex
     }
-
-
+    
 }
-
